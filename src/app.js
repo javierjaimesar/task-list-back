f7fc04503e948f6b86243ed0ad033e68d5f5d600
Javier Jaimes <javierjaimes2003@gmail.com>
2023-11-01 18:40:23 -0300

    cambio cors

--- src/app.js
+++ src/app.js
@@ -16,11 +16,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions)); 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
-
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cookieParser());
