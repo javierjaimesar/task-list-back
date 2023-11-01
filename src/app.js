791447cb7fcb994ab742a269c1961585977de76d
Javier Jaimes <javierjaimes2003@gmail.com>
2023-11-01 20:44:23 -0300

    otro cambiooo

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
-
 import authRoutes from './routes/auth.routes.js';
 import taskRoutes from './routes/task.routes.js';
 
@@ -21,7 +19,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cookieParser());
 app.use('/api', authRoutes)
 app.use('/api', taskRoutes)
 
