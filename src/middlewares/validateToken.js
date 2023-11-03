f8404fba6b4bf2fbfa2ba23f91b9aede914ded81
Javier Jaimes <javierjaimes2003@gmail.com>
2023-11-03 17:11:58 -0300

    Prueba async await en validateToken

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,21 +1,25 @@
 import jwt from 'jsonwebtoken';
 import { TOKEN_SECRET } from '../config.js';
 
-export const authRequire = (req, res, next) => {
-    const { token } = req.cookies;
+export const authRequire = async (req, res, next) => {
+    try {
+        const { token } = await req.cookies;
 
-    console.log(token);
+        console.log(token);
 
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized validateToken' });
+        if (!token) {
+            return res.status(401).json({ message: 'Unauthorized validateToken' });
+        }
+
+        jwt.verify(token, TOKEN_SECRET, (err, user) => {
+            if (err) return res.status(403).json({ message: 'Forbidden' })
+
+            req.user = user;
+
+            next();
+        })
+    } catch (error) {
+        console.log(error);
     }
-
-    jwt.verify(token, TOKEN_SECRET, (err, user) => {
-        if (err) return res.status(403).json({ message: 'Forbidden' })
-
-        req.user = user;
-
-        next();
-    })
 
 }
