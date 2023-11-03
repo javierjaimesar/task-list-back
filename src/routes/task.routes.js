a5091486ae0bc4013ecd53a19325f4be10805e41
Javier Jaimes <javierjaimes2003@gmail.com>
2023-11-03 17:25:28 -0300

    Eliminacion de validationToken

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
-import { authRequire } from '../middlewares/validateToken.js';
+// import { authRequire } from '../middlewares/validateToken.js';
 import { getTasks, getTask, createTask, deleteTask, updateTask } from '../controllers/task.controller.js';
 import { validateSchema } from '../middlewares/validator.middleware.js';
 import { createTaskSchema } from '../validations/task.schema.js';
 
 const router = Router();
 
-router.get('/task', authRequire, getTasks)
-router.get('/task/:id', authRequire, getTask)
-router.post('/task', authRequire, validateSchema(createTaskSchema), createTask)
-router.delete('/task/:id', authRequire, deleteTask)
-router.put('/task/:id', authRequire, updateTask)
+router.get('/task', getTasks)
+router.get('/task/:id', getTask)
+router.post('/task', validateSchema(createTaskSchema), createTask)
+router.delete('/task/:id', deleteTask)
+router.put('/task/:id', updateTask)
 
 export default router
